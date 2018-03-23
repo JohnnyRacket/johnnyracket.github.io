a0cc198bb31f28cc4e39a91bccc4b2c648cf28e7
John Harrison <john.randolph.harrison@gmail.com>
2018-03-23 01:31:28 -0500

    more about me tweaks

--- src/About/About.js
+++ src/About/About.js
@@ -13,17 +13,17 @@ export default function About (props){
                     To me, the fact that all these games were so simple but so fun is what drew me in. 
                     I always wanted to learn ActionScript and make my own flash game. I tried a couple times, but I only learned enough to make of of those "don't let your mouse touch the wall" type games. I distinctly remember being very confused on the concept of loops, which as it turns out, is pretty core to games!
                     <br/><br/>
-                    In college I spent a year as a microbiology major, I knew almost immediately that lots of school was now for me. So I started looking around at other majors and found myself once again enchanted with the idea that I could make things if I could code. 
+                    In college I spent a year as a microbiology major, but I knew almost immediately that lots of school was not for me. So I started looking around at other majors and found myself once again enchanted with the idea that I could make things if I could code. 
                     I have never looked back on the decision to switch to computer science and making things has been what I have loved doing ever since! 
                     A funny story I like to tell is that as an avid Runescape player in my early days I could type incredibly fast with two fingers, especially from "merching" stuff. But feeling devoted to learning to code, I had to force myself to learn how to type correctly before fully switching my major because I thought it would be absurd to be a programmer that "hunts and pecks". 
                     <br/><br/>
-                    College was great and I learned a ton of knowledge from some great professors and accomplished some cool things. One of the most memorable being winning my schools hackathon, SwampHacks 2015. 
+                    College was great and I learned so much from some great professors. One of the most memorable accomplishments was winning my schools hackathon, SwampHacks 2015, with (people who are now) my friends. We showed up only knowing a couple other people, and worked together to build a system that scraped our schools class schedule and would help you optimize your schedule.
                     Afterwards, I went out into the workforce, and somewhat ironically, I do just as much learning now as I did in college.
                     <br/><br/>
                     My curiosity and want to know more leads me to reading a lot more than I used to. 
-                    I am never done learning, and as a result, I love exploring and implementing; from microservice architectures, event sourcing systems, to building game engines and games from scratch I am always on a project.
+                    I feel like I am never done learning, there is always something I want to understand better. This leads me to trying a lot fo stuff out; from microservice architectures, event sourcing systems, to building game engines and games from scratch.
                     <br/><br/>
-                    <strong>Anyways, enough about me, enjoy the HTML5/Typescript game I built from scratch to fufill some of my goals!<br/> <a href="https://github.com/JohnnyRacket/playmaker" target="_blank">https://github.com/JohnnyRacket/playmaker</a></strong>
+                    <strong>Anyways, enough about me, enjoy the HTML5/Typescript game I built from scratch to achieve some of my goals! Its embedded in this page! <br/> <a href="https://github.com/JohnnyRacket/playmaker" target="_blank">https://github.com/JohnnyRacket/playmaker</a></strong>
                 </div>
 
                 <Game/>
